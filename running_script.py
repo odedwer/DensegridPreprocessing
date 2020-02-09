148eeddecc7666520b3aabe1643be1f6a2cfbdd1
Galche <galchen2@gmail.com>
2020-02-09 15:06:33 +0200

    expanded script for writing exclusions and full ica analysis

---
+++ running_script.py
@@ -0,0 +1,35 @@
+from preprocess_utilities import *
+import sys
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+components = range(20) # number of components to show
+
+if __name__ == "__main__":
+    mpl.use("TkAgg")
+    raw = mne.io.read_raw_fif(sys.argv[1])
+    raw.set_montage(montage=mne.channels.make_standard_montage('biosemi256', head_size=0.089), raise_if_subset=False)
+    print("plotting full psd...")
+    raw.plot_psd(fmin=0,fmax=300,picks=range(20),n_fft=10*2048)
+    plt.show()
+   #print("plotting short psd...")
+    #raw.plot_psd(fmin=0,fmax=30,picks=range(20),n_fft=10*2048)
+    #plt.show()
+    ica = mne.preprocessing.read_ica(sys.argv[2])
+    print("plotting components...")
+    ica.plot_components(outlines='skirt', picks=components, show=False)
+    plt.show()
+    print("plotting properties...")
+    comp_jumps = np.linspace(0, ica.n_components_, int(ica.n_components_ / 10) + 1) ##the begining of each components group to be shown
+    for i in range(len(comp_jumps)): ##go over the components and show 10 each time
+            comps = range(int(comp_jumps[i]), int(comp_jumps[i + 1]))
+            ica.plot_properties(raw, picks=comps, show=False)
+    plt.show()
+    print("plotting components correlations")
+    eog_indices, eog_scores = ica.find_bads_eog(raw)
+    ica.plot_scores(eog_scores, title="Nose correlations")
+    eog_indices, eog_scores = ica.find_bads_eog(raw, ch_name="LHEOG-RHEOG", threshold=2.5)
+    ica.plot_scores(eog_scores, title="Horizontal eye correlations")
+    eog_indices, eog_scores = ica.find_bads_eog(raw, ch_name="RVEOGS-RVEOGI")
+    ica.plot_scores(eog_scores, title="Vertical eye correlations")
+    plt.show()
