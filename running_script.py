b30ea13fa36f11c5fe7091681a0eccff5ab74d85
Galche <galchen2@gmail.com>
2020-02-09 15:27:59 +0200

    expanded script for writing exclusions and full ica analysis

--- running_script.py
+++ running_script.py
@@ -12,20 +12,25 @@ if __name__ == "__main__":
     print("plotting full psd...")
     raw.plot_psd(fmin=0,fmax=300,picks=range(20),n_fft=10*2048)
     plt.show()
-   #print("plotting short psd...")
-    #raw.plot_psd(fmin=0,fmax=30,picks=range(20),n_fft=10*2048)
-    #plt.show()
+    # print("plotting short psd...")
+    # raw.plot_psd(fmin=0,fmax=30,picks=range(20),n_fft=10*2048)
+    # plt.show()
     ica = mne.preprocessing.read_ica(sys.argv[2])
     print("plotting components...")
     ica.plot_components(outlines='skirt', picks=components, show=False)
     plt.show()
     print("plotting properties...")
     comp_jumps = np.linspace(0, ica.n_components_, int(ica.n_components_ / 10) + 1) ##the begining of each components group to be shown
-    for i in range(len(comp_jumps)): ##go over the components and show 10 each time
+    for i in range(len(comp_jumps)): # go over the components and show 10 each time
             comps = range(int(comp_jumps[i]), int(comp_jumps[i + 1]))
+            print("plotting from component "+str(comps))
             ica.plot_properties(raw, picks=comps, show=False)
-    plt.show()
+            plt.show()
+            if input("stop plotting? (Y/N)") == "Y":
+                break
     print("plotting components correlations")
+    raw = add_bipolar_derivation(raw, 'LHEOG', 'RHEOG')
+    raw = add_bipolar_derivation(raw, 'RVEOGS', 'RVEOGI')
     eog_indices, eog_scores = ica.find_bads_eog(raw)
     ica.plot_scores(eog_scores, title="Nose correlations")
     eog_indices, eog_scores = ica.find_bads_eog(raw, ch_name="LHEOG-RHEOG", threshold=2.5)
